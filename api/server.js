833ec7ac8ce99cfbf98f76e4be1ed2a6fdd1d202
Alan Kordel <alan.kordel@outlook.com.br>
2026-03-07 22:15:52 -0300

    feat: conectado API ao Posgree SQL Database

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const pool = require("./db/connection");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Orders API rodando");
+app.get("/", async (req, res) => {
+  try {
+
+    const result = await pool.query('SELECT * FROM Orders');
+
+    res.json(result.rows);
+
+  } catch (error) {
+
+    console.error(error);
+    res.status(500).send("Database error");
+
+  }
 });
 
 app.listen(3000, () => {
